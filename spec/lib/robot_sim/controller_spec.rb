af65883486d767dc55a998700c42e685222e3166
Patrick Cunnane <pat@Patricks-MacBook-Pro.local>
2017-03-08 15:46:36 -0800

    raise exception for size not given

--- spec/lib/robot_sim/controller_spec.rb
+++ spec/lib/robot_sim/controller_spec.rb
@@ -1,9 +1,20 @@
 require "spec_helper"
 
 RSpec.describe RobotSim::Controller do
-  context 'size not given first' do
-    it "raises the correct exception" do
-      expect { subject.execute("anything except size") }.to raise_exception(RobotSim::Controller::SizeNotGivenException)
+  describe 'size' do
+    context 'size not given first' do
+      it "raises the correct exception" do
+        expect { subject.execute("anything except size") }.to raise_exception(RobotSim::Controller::SizeNotGivenException)
+      end
     end
+
+    # context 'size given first' do
+    #   before(:each) do
+    #     subject.execute("size 3")
+    #   end
+    #   it "raises the correct exception" do
+    #     expect { subject.execute("anything except size") }.to raise_exception(RobotSim::Controller::SizeNotGivenException)
+    #   end
+    # end
   end
 end
